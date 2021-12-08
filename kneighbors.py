a5c87c65af4b96cd9c4be3d953fbc01882ab14ac
Giacomo De Lazzari <gdelazzari@aol.com>
2021-12-08 13:37:08 +0100

    kneighbors: compute Shapley values
    Add the same code we use in logistic.py to compute the Shapley values for each feature using the KNeighborsClassifier.
    
    This classifier seems to perform just as well as the logistic regression, but takes more time to fit and score.
    
    Related to #4

--- kneighbors.py
+++ kneighbors.py
@@ -4,14 +4,32 @@ from sklearn.neighbors import KNeighborsClassifier
 
 import datasets
 import utils
+from shapley import tmc_shapley
+import matplotlib.pyplot as plt
 
 
 X, Y = datasets.agaricus_lepiota()
-X, Y = utils.shuffle(X, Y)
-(X_train, Y_train), (X_test, Y_test) = utils.split(X, Y, 0.8)
 
-knc = KNeighborsClassifier(n_neighbors=5)
+(X_train, Y_train), (X_test, Y_test) = utils.prepare_dataset(X, Y, 0.8)
 
-knc.fit(X_train, Y_train)
+print(f"Using {len(Y_train)} training samples")
+print(f"Using {len(Y_test)} test samples")
+print(f"(using {len(Y_train) + len(Y_test)} samples out of {len(Y)} available)")
 
-print(knc.score(X_test, Y_test))
+c = KNeighborsClassifier(n_neighbors=5)
+v = lambda c, X_train, Y_train, X_test, Y_test: c.fit(X_train, Y_train).score(X_test, Y_test)
+
+vD = v(c, X_train, Y_train, X_test, Y_test)
+print(f"Score of whole ensemble of features: {vD}\n")
+
+print("TMC-Shapley:")
+sh = tmc_shapley(X_train, Y_train, X_test, Y_test, c, v)
+plt.barh(y=np.arange(len(sh)), width=list(sh))
+plt.show()
+
+# Check on Shapley values: sum must be equal to the total ensemble score
+# Check the distribution also
+print(f"\nSum of all Shapley values: {np.sum(sh)}\n")
+n_bins = 20
+plt.hist(sh, bins=n_bins)
+plt.show()
