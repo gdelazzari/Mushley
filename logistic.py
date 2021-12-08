24babee70ceda1ebe1b35f010d20956289fad187
Giacomo De Lazzari <gdelazzari@aol.com>
2021-12-08 11:41:38 +0100

    logistic.tmc_shapley: simplify code, take more settings
    Keeping the `v` array was not needed, we just need to keep the previous `v` value (now in `v_prev`.
    
    The function also now accepts the number of samples to compute (which by default are still left as `2*n`) and the initial value for `v`.

--- logistic.py
+++ logistic.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
-def tmc_shapley(X_train, Y_train, X_test, Y_test, perf_tolerance: float = 0.01):
+def tmc_shapley(X_train, Y_train, X_test, Y_test, n_samples = None, perf_tolerance: float = 0.01, v_init = 0.0):
     n = np.shape(X_train)[1]
 
     print(f"Number of features: {n}")
@@ -23,8 +23,12 @@ def tmc_shapley(X_train, Y_train, X_test, Y_test, perf_tolerance: float = 0.01):
     # initialize the vector of shapley values for each feature
     shapley = np.zeros(n)
 
+    # if `n_samples` is set to None, use 2n as the default`
+    if n_samples is None:
+        n_samples = 2 * n
+
     try:
-        for t in tqdm(range(1, 2*n), desc="samples", position=0, smoothing=0.0):
+        for t in tqdm(range(1, n_samples+1), desc="samples", position=0, smoothing=0.0):
             # obtain a permutation of the features, represented as a vector
             # of indexes into the columns of X_train and X_test
             perm = np.arange(n)
@@ -37,16 +41,17 @@ def tmc_shapley(X_train, Y_train, X_test, Y_test, perf_tolerance: float = 0.01):
             v = np.zeros(n + 1)
 
             # NOTE: is this correct? Shouldn't 50% accuracy be assumed?
-            v[0] = 0 # suppose to have zero accuracy with no training features
+            v_prev = v_init # suppose to have zero accuracy with no training features
 
             for j in tqdm(range(1, n + 1), desc="subsets", position=1, leave=False):
-                if abs(vD - v[j - 1]) < perf_tolerance:
-                    v[j] = v[j - 1]
+                if abs(vD - v_prev) < perf_tolerance:
+                    v = v_prev
                 else:
                     lr.fit(perm_X_train[:, :j], Y_train)
-                    v[j] = lr.score(perm_X_test[:, :j], Y_test)
+                    v = lr.score(perm_X_test[:, :j], Y_test)
 
-                shapley[perm[j - 1]] = (t - 1) / t * shapley[perm[j - 1]] + (v[j] - v[j - 1]) / t
+                shapley[perm[j - 1]] = (t - 1) / t * shapley[perm[j - 1]] + (v - v_prev) / t
+                v_prev = v
     
     except KeyboardInterrupt:
         # Allow to break early with Ctrl+C.
