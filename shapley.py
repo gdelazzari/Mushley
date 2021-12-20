shapley.tmc_shapley: fix variance @ early termination
The computation of the variance was not correct for the early termination case.
We should divide by the number of samples that we actually have, instead of the number of total samples we were aiming at collecting.

shapley.py
@@ -139,7 +139,7 @@ def tmc_shapley(
     shapley_var = np.zeros(n)
     for i in range(n):
         mu = shapley[i]
-        shapley_var[i] = np.var(np.array(shapley_chi[i]) - mu) / n_samples
+        shapley_var[i] = np.var(np.array(shapley_chi[i]) - mu) / len(shapley_chi[i])
 
     if save_results:
         basename = f"{n}-{n_samples}-{perf_tolerance}-{v_init}"